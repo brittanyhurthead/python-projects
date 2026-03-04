04f8ba8c9b994e1305e6451e0b02d58e14574539
brittanyhurthead <brittanyhurthead@gmail.com>
2026-03-03 19:26:55 -0500

    Added input validation

--- number_guess_game.py
+++ number_guess_game.py
@@ -20,7 +20,12 @@ secret_number = random.randint(1,max_number)
 attempts = 0
 
 while attempts < max_attempts:
-    guess = int(input(f"Guess a number between 1 and {max_number}:"))
+    try:
+        guess = int(input(f"Guess a number between 1 and {max_number}:"))
+    except ValueError:
+        print("Please enter a valid number.")
+    continue
+
     attempts += 1
 
     if guess < secret_number:
